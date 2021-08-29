2866383fe906ffe54a1bf3fad408bf985590446c
mahesh <mail.mahesh.lk@gmail.com>
2021-08-29 14:48:28 +0800

    higer order function

--- filterfn.go
+++ filterfn.go
@@ -1,3 +1,21 @@
 package main
 
+import "reflect"
+
 type filterf func(interface{}) bool
+
+//Filter(slice, predicate func)
+func Filter(in interface{}, fn filterf) interface{} {
+	val := reflect.ValueOf(in)
+	out := make([]interface{}, 0, val.Len())
+
+	for i := 0; i < val.Len(); i++ {
+		current := val.Index(i).Interface()
+
+		if fn(current) {
+			out = append(out, current)
+		}
+	}
+
+	return out
+}
--- reducefn.go
+++ reducefn.go
@@ -1,3 +1,16 @@
 package main
 
+import "reflect"
+
 type reducef func(interface{}, interface{}) interface{}
+
+//Reduce(slice, starting value, func)
+func Reduce(in interface{}, memo interface{}, fn reducef) interface{} {
+	val := reflect.ValueOf(in)
+
+	for i := 0; i < val.Len(); i++ {
+		memo = fn(val.Index(i).Interface(), memo)
+	}
+
+	return memo
+}
