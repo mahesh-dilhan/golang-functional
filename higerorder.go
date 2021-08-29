2898d541216a9936d5ef671f9a2cc83b91fc5d9b
mahesh <mail.mahesh.lk@gmail.com>
2021-08-29 14:36:55 +0800

    higer order function

--- higerorder.go
+++ higerorder.go
@@ -12,6 +12,32 @@ func main() {
 	}
 
 	caller(f)
+
+	square := func(x interface{}) interface{} {
+		return x.(int) * x.(int)
+	}
+	nums := []int{1, 2, 3, 4}
+
+	fmt.Println(nums)
+	gnums := make([]interface{}, len(nums))
+	for i, x := range nums {
+		gnums[i] = x
+	}
+	fmt.Println(gnums)
+	gsquared := Map(square, gnums)
+	squared := make([]int, len(gsquared))
+	for i, x := range gsquared {
+		squared[i] = x.(int)
+	}
+	fmt.Println(squared)
+}
+
+func Map(f func(interface{}) interface{}, xs []interface{}) []interface{} {
+	ys := make([]interface{}, len(xs))
+	for i, x := range xs {
+		ys[i] = f(x)
+	}
+	return ys
 }
 
 func caller(f func(str string) string) {
