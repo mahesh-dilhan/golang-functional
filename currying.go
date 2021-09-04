1ae8f2a2df5f3f1f49614b7462c952930e6098c6
mahesh <mail.mahesh.lk@gmail.com>
2021-09-04 21:07:08 +0800

    higer order function

---
+++ currying.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func plus(x, y int) int {
+	return x + y
+}
+func partialPlus(x int) func(int) int {
+	return func(y int) int {
+		return plus(x, y)
+	}
+}
+func main() {
+	plus_one := partialPlus(1)
+	fmt.Println(plus_one(5)) //prints 6
+}
