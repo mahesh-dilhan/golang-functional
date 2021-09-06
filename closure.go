983bd5aaa6a84ba1295e542eb6404c5034fc569d
mahesh <mail.mahesh.lk@gmail.com>
2021-09-06 21:33:07 +0800

    higer order function

--- closure.go
+++ closure.go
@@ -20,4 +20,20 @@ func main() {
 	f2() // 2
 	f1() // 2
 	f2() // 2
+
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
+}
+
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
 }
